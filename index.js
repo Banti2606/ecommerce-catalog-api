d298ff741e0240900d8066c709fb5aa0031ac96b
Banti <banti152666@gmail.com>
2026-03-06 21:22:14 +0530

    fix vercel serverless deployment

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ require("dotenv").config();
 const connectDB = require("./src/config/db");
 const app = require("./src/app");
 
+// connect database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// DO NOT use app.listen() on Vercel
 
 module.exports = app;
